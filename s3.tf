a4a8d763ac5ee99ccf9dc30ec0bf3d40381b45f4
Guilherme Fujii <gui.fujii@outlook.com>
2025-05-16 23:51:43 +0000

    fix: remove acesso publico no bucket

--- s3.tf
+++ s3.tf
@@ -8,3 +8,10 @@ resource "aws_s3_bucket_versioning" "versioning_bucket" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "versioning_bucket_access" {
+  bucket = aws_s3_bucket.versioning_bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
